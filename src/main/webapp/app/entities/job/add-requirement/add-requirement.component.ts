31f76aeddef0b1dd4b7323018505db565dc1cf76
Eric <eric.earlywine@hotmail.com>
2020-04-05 21:38:56 -0500

    Working on adding requirements

---
+++ src/main/webapp/app/entities/job/add-requirement/add-requirement.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-add-requirement',
+  templateUrl: './add-requirement.component.html',
+  styleUrls: ['./add-requirement.component.scss']
+})
+export class AddRequirementComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/main/webapp/app/entities/job/job.module.ts
+++ src/main/webapp/app/entities/job/job.module.ts
@@ -7,10 +7,11 @@ import { JobDetailComponent } from './job-detail.component';
 import { JobUpdateComponent } from './job-update.component';
 import { JobDeleteDialogComponent } from './job-delete-dialog.component';
 import { jobRoute } from './job.route';
+import { AddRequirementComponent } from './requirements/add-requirement/add-requirement.component';
 
 @NgModule({
   imports: [OddJobsSharedModule, RouterModule.forChild(jobRoute)],
-  declarations: [JobComponent, JobDetailComponent, JobUpdateComponent, JobDeleteDialogComponent],
+  declarations: [JobComponent, JobDetailComponent, JobUpdateComponent, JobDeleteDialogComponent, AddRequirementComponent],
   entryComponents: [JobDeleteDialogComponent]
 })
 export class OddJobsJobModule {}
